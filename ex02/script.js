7eade18ac9a412d3d9d6799e513952684e7d34e6
denzelana-ship-it <denzel.ana@edu.senai.br>
2026-04-01 12:14:52 -0300

    legal

--- ex02/script.js
+++ ex02/script.js
@@ -3,5 +3,5 @@ let numero = Number(prompt("digite sua idade:"));
 if (numero >= 18) {
     alert("você e maior de idade");
 } else{
-    alert("você e menor de idade")
+    alert("você e menor de idade.")
 }
